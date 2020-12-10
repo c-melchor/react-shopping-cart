e1a22ad82a99a7decb8101250aeb8470c970a08f
c_melchor <cah.melchor@gmail.com>
2020-12-10 13:59:04 -0800

    cart updates with each item

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,11 @@
 import React, { useContext } from "react";
-import ProductContext from "../contexts/ProductContext";
+import { ProductContext } from "../contexts/ProductContext";
 
 // Components
 import Product from "./Product";
 
-const Products = props => {
+const Products = () => {
   const { products, addItem } = useContext(ProductContext);
-  console.log("PRODUCTS.JS", products);
   return (
     <div className="products-container">
       {products.map(product => (
